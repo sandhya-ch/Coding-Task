4723968a2f0eacfaa638a8e94bcd5d2d505b4752
sandhya-ch <sandhyareddych1997@gmail.com>
2020-08-16 19:23:24 +0530

    file upload logic implemented in views

--- myapp/views.py
+++ myapp/views.py
@@ -2,9 +2,32 @@ from django.shortcuts import render
 from myapp.serializers import PersonSerializers
 from rest_framework import viewsets
 from myapp.models import Person
-
+import pandas as pd
+from myapp.models import Person
 
 # Create your views here.
 class Personviewset(viewsets.ModelViewSet):
     serializer_class=PersonSerializers
     queryset=Person.objects.all()
+
+    def create(self, request):
+        data = request.data
+        name = data.get('name',None)
+        age = data.get('age',None)
+        designation = data.get('designation',None)
+        file=request.FILES.get('file',None)
+        if file:
+            csv_data=pd.read_csv(file)
+            print(csv_data)
+            personObjs = [
+                Person(
+                    name = row['Name'],
+                    age = row['Age'],
+                    designation = row['Designation']
+                )
+                for index,row in csv_data.iterrows()
+            ]
+            Person.objects.bulk_create(personObjs)
+        else:
+            Person.objects.create(name=name,age=age,designation=designation)
+        return Response({"message":'Successfully data inserted'})
