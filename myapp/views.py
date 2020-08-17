2b1db9b82f87a599eada1976cf6b67cb71d4412f
sandhya-ch <sandhyareddych1997@gmail.com>
2020-08-17 14:17:09 +0530

    UI implemented for file upload

--- myapp/views.py
+++ myapp/views.py
@@ -3,8 +3,10 @@ from myapp.serializers import PersonSerializers
 from rest_framework import viewsets
 from myapp.models import Person
 import pandas as pd
-from rest_framework.response import Response
+from django.http import HttpResponse
 from rest_framework import permissions
+from .forms import UploadFileForm
+
 
 # Create your views here.
 class Personviewset(viewsets.ModelViewSet):
@@ -12,13 +14,11 @@ class Personviewset(viewsets.ModelViewSet):
     queryset=Person.objects.all()
     permission_classes = [permissions.AllowAny]
 
-    def create(self, request):
-        data = request.data
-        name = data.get('name',None)
-        age = data.get('age',None)
-        designation = data.get('designation',None)
-        file=request.FILES.get('file',None)
-        if file:
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file=request.FILES['file']
             csv_data=pd.read_csv(file)
             personObjs = [
                 Person(
@@ -29,6 +29,7 @@ class Personviewset(viewsets.ModelViewSet):
                 for index,row in csv_data.iterrows()
             ]
             Person.objects.bulk_create(personObjs)
-        else:
-            Person.objects.create(name=name,age=age,designation=designation)
-        return Response({"message":'Successfully data inserted'})
+            return HttpResponse("<h1>Successfully data inserted</h1>")
+    else:
+        form = UploadFileForm()
+    return render(request, 'index.html', {'form': form})
